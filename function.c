011621fc8121a04cd27b7543f3bda5149f2ee07f
Isaac Rhett <isaacjrhett@gmail.com>
2017-06-27 22:38:22 -0700

    resolved compilation issues

--- function.c
+++ function.c
@@ -6,8 +6,13 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 21:53:40 by irhett            #+#    #+#             */
-/*   Updated: 2017/06/27 21:57:20 by irhett           ###   ########.fr       */
+/*   Updated: 2017/06/27 22:34:24 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "trash_grid.h"
+
+void	function(void)
+{
+	return ;
+}
