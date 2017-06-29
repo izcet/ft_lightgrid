2e3616403e907b978f1250df1750e0826971bb46
Isaac Rhett <isaacjrhett@gmail.com>
2017-06-28 22:07:29 -0700

    added libft, starting letter generation

--- lettergen.sh
+++ lettergen.sh
@@ -1,11 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR=alphabet
-VAR="a"
+VAR="A" #_
 
-while [ 1 ] ; do
-	echo "$VAR -> \c"
-	VAR=$(echo $VAR | tr "a-z" "a-z_")
-	echo "$VAR"
+
+function handle_letter() {
+	letter=$1
+	file="letter_$letter.c"
+	width=$(head -1 $DIR/$letter)
+	length=8
+	w=0;
+	l=0;
+
+	echo "starting letter $letter"
+
+	echo "#include trash_grid.h\n" >> $file
+	echo "void\tletter_$letter(t_letter **l)\n{" >> $file
+	sh echo "\tint\t\tetter;" >> $file
+	echo "\tint\t\ti;" >> $file
+	echo "" >> $file
+	echo "\tetter = letter_pos('$letter');" >> $file
+	echo "\tl[etter] = (t_letter*)malloc(sizeof(t_letter));" >> $file
+	echo "\tl[etter]->width = $width;" >> $file
+	echo "\tl[etter]->dots = (char**)malloc(sizeof(char*) * $length);" >> $file
+	while [ "$l" -lt "$length" ] ; do
+		echo "\tl[etter]->dots[$l] = ft_strnew($width + 1);" >> $file
+		line="$(head -$(($l + 1)) $DIR/$letter | tail -n 1)"
+		echo "\tft_memcpy(l[etter]->dots[$l], \"$LINE\", $width);" >> $file
+		l=$(($l + 1))
+	done
+	echo "}" >> $file
+
+
+	echo "void\t\t\tletter_${letter}(t_letter **l);" >> trash_grid.h
 	sleep .1
+}
+
+
+while [ "$VAR" != "_" ] ; do
+	handle_letter $VAR
+
+	#echo -n "$VAR -> "
+	
+	VAR=$(echo $VAR | tr "0-9A-Z" "1-9A-Z_")
+	
+	#echo "$VAR"
+	#sleep .1
 done
--- trash_grid.h
+++ trash_grid.h
@@ -5,25 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/27 21:56:27 by irhett            #+#    #+#             */
-/*   Updated: 2017/06/27 22:38:10 by irhett           ###   ########.fr       */
+/*   Created: 2017/06/28 21:05:22 by irhett            #+#    #+#             */
+/*   Updated: 2017/06/28 21:39:39 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TRASH_GRID_H
 # define TRASH_GRID_H
 
+#include "libft.h"
 
-typedef struct  s_grid
+typedef struct	s_grid
 {
-	int           **pixels;
-	int           width;
-	int           height;
-}               t_grid;
+	int			**pixels;
+	int			width;
+	int			height;
+}				t_grid;
 
-t_grid		grid;
+typedef struct	s_letter
+{
+	int 		width;
+	char		**dots;
+}				t_letter;
 
 
-void	function(void);
+
+
+t_grid 			grid;
+
+
+void			function(void);
+
 
 #endif
+void\t\t\tletter_A(t_letter **l);
+void\t\t\tletter_B(t_letter **l);
+void\t\t\tletter_C(t_letter **l);
+void\t\t\tletter_D(t_letter **l);
+void\t\t\tletter_E(t_letter **l);
+void\t\t\tletter_F(t_letter **l);
+void\t\t\tletter_G(t_letter **l);
+void\t\t\tletter_H(t_letter **l);
+void\t\t\tletter_I(t_letter **l);
+void\t\t\tletter_J(t_letter **l);
+void\t\t\tletter_K(t_letter **l);
+void\t\t\tletter_L(t_letter **l);
+void\t\t\tletter_M(t_letter **l);
+void\t\t\tletter_A(t_letter **l);
+void\t\t\tletter_B(t_letter **l);
+void\t\t\tletter_C(t_letter **l);
+void\t\t\tletter_D(t_letter **l);
+void\t\t\tletter_E(t_letter **l);
+void\t\t\tletter_F(t_letter **l);
+void\t\t\tletter_G(t_letter **l);
+void\t\t\tletter_H(t_letter **l);
+void\t\t\tletter_I(t_letter **l);
+void\t\t\tletter_J(t_letter **l);
+void\t\t\tletter_K(t_letter **l);
+void\t\t\tletter_L(t_letter **l);
+void\t\t\tletter_M(t_letter **l);
+void\t\t\tletter_N(t_letter **l);
+void\t\t\tletter_O(t_letter **l);
+void\t\t\tletter_P(t_letter **l);
+void\t\t\tletter_Q(t_letter **l);
+void\t\t\tletter_R(t_letter **l);
+void\t\t\tletter_S(t_letter **l);
+void\t\t\tletter_T(t_letter **l);
+void\t\t\tletter_U(t_letter **l);
+void\t\t\tletter_V(t_letter **l);
+void\t\t\tletter_W(t_letter **l);
+void\t\t\tletter_X(t_letter **l);
+void\t\t\tletter_Y(t_letter **l);
+void\t\t\tletter_Z(t_letter **l);
